a3f73eb112da09f9f996d146bdf5252676258f08
satto <andrea.borsatto@mail.polimi.it>
2021-04-28 10:36:32 +0200

    Fixed LateralWheelSync.cpp

--- src/LateralWheelSync.cpp
+++ src/LateralWheelSync.cpp
@@ -3,8 +3,6 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
-typedef message_filters::sync_policies
-    ::ApproximateTime<robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed> MySyncPolicy;
 
 class LateralWheelSync
 {
@@ -18,25 +16,28 @@ private:
     message_filters::Subscriber<robotics_hw1::MotorSpeed> sub1;
     message_filters::Subscriber<robotics_hw1::MotorSpeed> sub2;
     ros::Publisher pub;
-    message_filters::Synchronizer<MySyncPolicy> sync;
-
+    typedef message_filters::sync_policies::ApproximateTime<robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed> MySyncPolicy;
+    //message_filters::Synchronizer<MySyncPolicy> sync;
+    typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+    boost::shared_ptr<Sync> sync;
 
 public:
     LateralWheelSync(){
-        sub1.subscribe(n, "/frontWheel", 10);
-        sub2.subscribe(n, "/rearWheel", 10);
+        sub1.subscribe(n, "/motor_speed_fl", 1);
+        sub2.subscribe(n, "/motor_speed_rl", 1);
         pub = n.advertise<robotics_hw1::MotorSpeed>("/syncVelocity", 1);
-        sync = message_filters::Synchronizer<MySyncPolicy>(MySyncPolicy(10), sub1, sub2);
-        sync.registerCallback(boost::bind(&syncCallback, _1, _2));
-
-
+        sync.reset(new Sync(MySyncPolicy(10), sub1, sub2));
+        //message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), sub1, sub2);
+        sync->registerCallback(boost::bind(&LateralWheelSync::syncCallback, this, _1, _2));
     }
 
     void syncCallback(const robotics_hw1::MotorSpeed::ConstPtr& msg1,
                   const robotics_hw1::MotorSpeed::ConstPtr& msg2) {
-        ROS_INFO ("Received two messages: (%f,%f,%f) and (%f,%f,%f)",
+        ROS_INFO ("syncCallback");
+        pub.publish(*msg1);
+        /*ROS_INFO ("Received two messages: (%f,%f,%f) and (%f,%f,%f)",
                   msg1->vector.x,msg1->vector.y,msg1->vector.z,
-                  msg2->vector.x, msg2->vector.y, msg2->vector.z);
+                  msg2->vector.x, msg2->vector.y, msg2->vector.z);*/
     }
 
 
