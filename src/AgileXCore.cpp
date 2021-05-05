f65e9fed3364fa87c345d6d2207ba1fa08319229
satto <andrea.borsatto@mail.polimi.it>
2021-05-05 22:17:36 +0200

    Added dynamic reconfigure with runge-kutta integration

--- src/AgileXCore.cpp
+++ src/AgileXCore.cpp
@@ -2,6 +2,8 @@
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 #include "std_msgs/String.h"
+#include <dynamic_reconfigure/server.h>
+#include "project_1/integrationConfig.h"
 
 /*class MyOdometry{
 
@@ -104,10 +106,11 @@ private:
     ros::Publisher odom_pub;
     tf::TransformBroadcaster odom_broadcaster;
 
+    int integrationType;
+
 
 public:
     Pub_sub_odometry() {
-        //TODO: mettere topic delle velocità
         sub = n.subscribe("/twist", 1, &Pub_sub_odometry::computeOdometry2, this);
 
         odom_pub = n.advertise<nav_msgs::Odometry>("/odometry", 1);
@@ -116,6 +119,7 @@ public:
         x = 0;
         y = 0;
         th = 0;
+        integrationType = 0;
     }
 
     /*void computeOdometry(const geometry_msgs::TwistStamped::ConstPtr& msg){
@@ -123,7 +127,7 @@ public:
     }*/
 
     void computeOdometry2(const geometry_msgs::TwistStamped::ConstPtr& msg){
-        double vx, /*vy,*/ w, dt;
+        double vx, w, dt;
         ros::Time currentTime;
 
         //Reads currentTime from message's header
@@ -131,29 +135,34 @@ public:
 
         //Computes dt from last message
         dt = (currentTime - lastTime).toSec();
-        //ROS_INFO("%lf", dt);
         //Computes reads linear and angular velocities from message
         vx = msg->twist.linear.x;
         //vy = msg->twist.linear.y;
         w = msg->twist.angular.z;
 
+        ROS_INFO("INT TYPE: %d", integrationType);
         //Integration
-        x += vx * cos(th) * dt;
-        y += vx * sin(th) * dt;
-        th += w * dt;
-
-        //ROS_INFO("vx:%lf, x:%lf, y:%lf, th:%lf",vx, x, y, th);
+        if(integrationType == 0) { //EULER
+            x += vx * cos(th) * dt;
+            y += vx * sin(th) * dt;
+            th += w * dt;
+        }
+        else if(integrationType == 1){ //RUNGE-KUTTA
+            x += vx * cos(th + w * dt / 2) * dt;
+            y += vx * sin(th + w * dt / 2) * dt;
+            th += w * dt;
+        }
 
         //Publish tf transformation
         publishTfTransformation(currentTime);
         //Publish odometry message
-        publishOdometry(vx, /*vy,*/ w, currentTime);
+        publishOdometry(vx, w, currentTime);
 
         //Updates last time
         lastTime= currentTime;
     }
 
-    void publishOdometry(double vx, /*double vy,*/ double w, ros::Time currentTime){
+    void publishOdometry(double vx, double w, ros::Time currentTime){
         nav_msgs::Odometry odometry;
         geometry_msgs::Quaternion odometryQuaternion = tf::createQuaternionMsgFromYaw(th);
 
@@ -192,11 +201,22 @@ public:
         //publish transformation
         odom_broadcaster.sendTransform(odometryTransformation);
     }
+
+    void setIntegration(project_1::integrationConfig &config){
+        integrationType = config.integration;
+    }
 };
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "AgileXCore");
     Pub_sub_odometry pubSubOdometry;
+
+    dynamic_reconfigure::Server<project_1::integrationConfig> server;
+    dynamic_reconfigure::Server<project_1::integrationConfig>::CallbackType f;
+
+    f = boost::bind(&Pub_sub_odometry::setIntegration, &pubSubOdometry, _1);
+    server.setCallback(f);
+
     ros::spin();
     return 0;
 }
